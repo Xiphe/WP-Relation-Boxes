728395e34e02361ef94265b9beae9996f1c85385
Hannes Diercks <xiphe@gmx.de>
2012-09-20 10:14:30 +0200

    fix

--- relationboxes.php
+++ relationboxes.php
@@ -1,13 +1,13 @@
-<?php //die('Symlinktest');
+<?php
 /*
 Plugin Name: Relation Boxes
-Plugin URI: http://red-thorn.de
-Description: 
+Plugin URI: https://github.com/Xiphe/WP-Relation-Boxes
+Description: A Wordpress Plugin for easy enabling n-1, 1-1 and n-n relationships
 Version: 0.0.5
 Namespace: Xiphe\RelationBoxes
 Date: 19.09.2012 17:30:00 +02:00
 Author: Xiphe
-Author URI: http://red-thorn.de
+Author URI: https://github.com/Xiphe/
 Required Plugins: !themaster, !html
 Updatable: true
 */
